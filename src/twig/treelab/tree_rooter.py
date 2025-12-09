91c58d56957adf7837eb360d3244c65bcfa5c615
deren <de2356@columbia.edu>
2025-12-09 18:57:11 -0500

    logging fix

--- src/twig/treelab/tree_rooter.py
+++ src/twig/treelab/tree_rooter.py
@@ -12,6 +12,7 @@ from pathlib import Path
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from loguru import logger
 import toytree
+from twig.utils.logger_setup import set_log_level
 
 
 KWARGS = dict(
@@ -101,7 +102,7 @@ def get_rooting_clades(sptree, outgroups):
 
 def run_tree_rooter(args):
     # require -s -r or -R
-    toytree.set_log_level(args.log_level)
+    set_log_level(args.log_level)
     assert args.trees.exists(), f"trees file {args.trees}..."
 
     # parse outfile args to list
